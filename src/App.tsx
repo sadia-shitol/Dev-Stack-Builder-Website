a2ba350bf469e367dbcc71427cedf7491d47c47a
SadiaShitol <shitol3080@gmail.com>
2026-09-13 00:07:32 +0600

    Initial Setup commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className='bg-red-500 p-10 text-3xl font-bold text-white'>
+        My React App
+      </h1>
+      <button className='btn btn-error'>Error</button>
+    </>
+  )
+}
+
+export default App
