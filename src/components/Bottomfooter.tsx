4eb4a688d0f9e2e49e30e8c810971e638c88c088
SadiaShitol <shitol3080@gmail.com>
2026-09-13 23:22:44 +0600

    minor changes

--- src/components/Bottomfooter.tsx
+++ src/components/Bottomfooter.tsx
@@ -1,6 +1,6 @@
 const Bottomfooter = () => {
   return (
-    <footer className=' mt-4 border-t border-[#e3e6e8]  bg-base-200 text-base-content footer flex pl-[400px] justify-start align-top sm:footer-horizontal items-center p-4 '>
+    <footer className='mt-0.5 border-t border-[#e3e6e8]  bg-base-200 text-base-content footer flex pl-[400px] justify-start align-top sm:footer-horizontal items-center p-4 '>
       <aside className='grid-flow-col items-start'>
         <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
       </aside>
