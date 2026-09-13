7136e282a394f4cc284b0646bc36c2d67a066d0a
SadiaShitol <shitol3080@gmail.com>
2026-09-13 19:14:04 +0600

    Added data.json file

--- src/components/Stackcards.tsx
+++ src/components/Stackcards.tsx
@@ -1,7 +1,4 @@
-import { use } from 'react'
-
-const Stackcards = ({ devStackCardPromise }) => {
-  const stackcards = use(devStackCardPromise)
+const Stackcards = () => {
   return <div></div>
 }
 
