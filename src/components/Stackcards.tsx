a8953d78d3301264f885a720491e67c0c6433865
SadiaShitol <shitol3080@gmail.com>
2026-09-13 20:39:50 +0600

    type declaration and other issues fixed

--- src/components/Stackcards.tsx
+++ src/components/Stackcards.tsx
@@ -1,10 +1,13 @@
 import { use } from 'react'
 import type { IstackType } from '../types/stacktype'
-interface StackcardsProps {
+
+type StackcardsProps = {
   stackcardsPromise: Promise<IstackType[]>
 }
+
 const Stackcards = ({ stackcardsPromise }: StackcardsProps) => {
   const stackcards = use(stackcardsPromise)
+  console.log(stackcards)
   return <div></div>
 }
 
