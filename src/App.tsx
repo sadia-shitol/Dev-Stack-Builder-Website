06a0fd34c6d87078f5786c5d7a99f0207d4236b6
SadiaShitol <shitol3080@gmail.com>
2026-09-13 09:16:10 +0600

    added footer portion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Banner from './components/Banner'
+import LowerFooter from './components/lowerFooter'
 import Navbar from './components/Navbar'
+import Prefooter from './components/Prefooter'
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Prefooter />
+      <LowerFooter />
     </>
   )
 }
---
+++ src/components/Prefooter.tsx
@@ -0,0 +1,42 @@
+import logo from '../assets/logo-text.png'
+const Prefooter = () => {
+  return (
+    <div className='container my-0'>
+      <footer className='footer sm:footer-horizontal border-b border-[#f1f5f9] bg-base-200 text-base-content p-10'>
+        <aside>
+          <img src={logo} alt='' />
+          <p>
+            Curated tools, technologies, and resources for developers building
+            <br />
+            modern software.
+          </p>
+
+          <ul className='flex gap-6'>
+            <li className='text-slate-600 font-semibold'>GitHub</li>
+            <li className='text-slate-600 font-semibold'>Twitter</li>
+            <li className='text-slate-600 font-semibold'>LinkedIn</li>
+          </ul>
+        </aside>
+        <nav>
+          <h6 className='footer-title  text-slate-700 '>PRODUCT</h6>
+          <a className='link link-hover text-slate-950'>Home</a>
+          <a className='link link-hover  text-slate-950'>Technologies</a>
+          <a className='link link-hover text-slate-950'>Projects</a>
+        </nav>
+        <nav>
+          <h6 className='footer-title  text-slate-700'>Company</h6>
+          <a className='link link-hover text-slate-950'>About</a>
+          <a className='link link-hover text-slate-950'>Contact</a>
+          <a className='link link-hover text-slate-950'>Careers</a>
+        </nav>
+        <nav>
+          <h6 className='footer-title  text-slate-700'>Legal</h6>
+          <a className='link link-hover text-slate-950'>Privacy policy</a>
+          <a className='link link-hover text-slate-950'>Terms of Service</a>
+        </nav>
+      </footer>
+    </div>
+  )
+}
+
+export default Prefooter
